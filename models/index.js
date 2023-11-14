cf5935533f7cee3be0bbd0160a1aad4075b7fc79
Wo-ogie <siwall0105@gmail.com>
2023-11-14 11:30:31 +0900

    chore: model 설정 파일 수정

--- models/index.js
+++ models/index.js
@@ -1,13 +1,12 @@
+const Sequelize = require('sequelize');
 const fs = require('fs');
 const path = require('path');
-const Sequelize = require('sequelize');
 
-const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
+// eslint-disable-next-line import/extensions
 const config = require('../config/config.js')[env];
 
 const db = {};
-
 const sequelize = new Sequelize(
   config.database,
   config.username,
@@ -15,20 +14,20 @@ const sequelize = new Sequelize(
   config,
 );
 
+db.sequelize = sequelize;
+
+const basename = path.basename(__filename);
 fs.readdirSync(__dirname)
   .filter(
     (file) =>
-      file.indexOf('.') !== 0 &&
-      file !== basename &&
-      file.slice(-3) === '.js' &&
-      file.indexOf('.test.js') === -1,
+      file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js',
   )
   .forEach((file) => {
-    const model = require(path.join(__dirname, file))(
-      sequelize,
-      Sequelize.DataTypes,
-    );
+    // eslint-disable-next-line import/no-dynamic-require
+    const model = require(path.join(__dirname, file));
+    console.log(file, model.name);
     db[model.name] = model;
+    model.initiate(sequelize);
   });
 
 Object.keys(db).forEach((modelName) => {
@@ -36,8 +35,5 @@ Object.keys(db).forEach((modelName) => {
     db[modelName].associate(db);
   }
 });
-
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
 
 module.exports = db;
