b85f96a9f798280c2d19f9dac7605e96ec58a924
Wo-ogie <siwall0105@gmail.com>
2023-11-14 13:53:01 +0900

    feat: 유저 entity 정의

---
+++ models/user.js
@@ -0,0 +1,40 @@
+const Sequelize = require('sequelize');
+
+class User extends Sequelize.Model {
+  static initiate(sequelize) {
+    User.init(
+      {
+        name: {
+          type: Sequelize.STRING(10),
+          allowNull: false,
+          unique: true,
+        },
+        password: {
+          type: Sequelize.STRING(100),
+          allowNull: true,
+        },
+        email: {
+          type: Sequelize.STRING(40),
+          allowNull: true,
+        },
+      },
+      {
+        sequelize,
+        timestamps: true,
+        underscored: true,
+        modelName: 'User',
+        tableName: 'users',
+        paranoid: false,
+        charset: 'utf8mb4',
+        collate: 'utf8mb4_general_ci',
+      },
+    );
+  }
+
+  static associate(db) {
+    db.User.belongsTo(db.Meeting);
+    db.User.hasMany(db.ScheduleVote);
+  }
+}
+
+module.exports = User;
