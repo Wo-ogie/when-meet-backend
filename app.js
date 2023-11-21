68087fe5dfa4c8942e9c02a5a9839685c76cba2a
Wo-ogie <siwall0105@gmail.com>
2023-11-21 11:20:29 +0900

    refactor: 에러 발생 시의 응답에 http status code 설정 로직 추가

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ app.use((req, res, next) => {
 });
 
 app.use((err, req, res, next) => {
-  res.json({
+  res.status(err.status || 500).json({
     message: err.message,
     error: process.env.NODE_ENV !== 'production' ? err : {},
   });
