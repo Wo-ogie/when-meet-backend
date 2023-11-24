c332286ee5145235cb0a9f0eec3e33611fb4c351
Wo-ogie <siwall0105@gmail.com>
2023-11-24 14:03:08 +0900

    chore: CORS 전체 허용 설정

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const morgan = require('morgan');
 const session = require('express-session');
@@ -32,6 +33,7 @@ app.use((req, res, next) => {
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(
   session({
