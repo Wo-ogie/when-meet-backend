230e64f700ecf413372a1a5bb4a80b566a65e2a9
Wo-ogie <siwall0105@gmail.com>
2023-11-24 17:45:35 +0900

    fix: 인증 권한 확인 시 cookie에 찾고자 하는 정보가 없을 경우 500에러가 발생하는 문제 수정

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,5 +1,8 @@
 exports.isAuthenticated = (req, res, next) => {
-  const participantData = JSON.parse(req.signedCookies.participantData);
+  let participantData = null;
+  if (req.signedCookies.participantData) {
+    participantData = JSON.parse(req.signedCookies.participantData);
+  }
   if (!participantData || participantData.meetingId !== req.params.meetingId) {
     const error = new Error('접근 권한이 없습니다.');
     error.status = 401;
